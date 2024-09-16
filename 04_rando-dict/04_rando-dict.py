d269775c0c008327d4b4e0f8f012543adab7a565
Sascha Gordon-Zolov <agordonzolov50@stuy.edu>
2024-09-16 11:27:05 -0400

    the odds of this working are zero -- have something down but trying to remember syntax

---
+++ 04_rando-dict/04_rando-dict.py
@@ -0,0 +1,27 @@
+%Sascha Gordon-Zolov
+%Team Sizzle
+%Softdev
+%K04:: Python Dictionaries and Random Selections
+%2024-9-16
+%Time spent:
+def randomDev():
+    krewes = {
+           4: [ 
+        'DUA','TAWAB','EVA','JACK','VICTOR','EVAN','JASON','COLYI','IVAN','TANZEEM',
+        'TAHMIM','STANLEY','LEON','NAOMI','NIA','ANASTASIA','JADY','BRIAN','JACOB',
+        'ALEX','CHONGTIAN','DANNY','MARCO','ABIDUR','ANKITA','ANDY','ETHAN','AMANDA',
+        'AIDAN','LINDA','QIANJUN','JIAYING','KISHI'
+        ],
+           5: [ 
+                'ADITYA','MARGIE','RACHEL','ALEXANDER','ZIYAD','DANNY','ENDRIT','CADEN',
+                'VEDANT','SUHANA','KYLE','KEVIN','RAYMOND','CHRISTOPHER','JONATHAN','SASHA',
+                'NAFIYU','TIM','WILL','DANIEL','BENJAMIN','CLAIRE','CHLOE','STELLA','TRACY',
+                'JESSICA','JACKIE','WEN YUAN','YINWEI','TIFFANY','JAYDEN DANIEL','PRINCEDEN' 
+              ]
+         }
+    pd = randint(4, 5)
+    leng = len(krewes[4])
+    indiv = randint(0, leng)
+    return krewes[pd][indiv]
+print(randomDev())
+    
