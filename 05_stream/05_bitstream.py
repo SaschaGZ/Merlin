d898a5065c9470d6414dddcb2d1e1580995af394
Sascha Gordon-Zolov <agordonzolov50@stuy.edu>
2024-09-17 11:47:29 -0400

    that works?? python is outrageous; made a double array of items

--- 05_stream/05_bitstream.py
+++ 05_stream/05_bitstream.py
@@ -7,5 +7,9 @@ Time spent: '''
 from random import randint
 def bitstream():
     file = open("krewes.txt", "r")
-    print(file.read())
+    file = file.read()
+    l1 = file.split("@@@")
+    for i in range (0, len(l1) - 1):
+        l1[i] = l1[i].split("$$$")
+    print(l1)
 bitstream()
