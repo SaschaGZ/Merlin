a8eca929dbf6d1dd006e5271c37b0f5f73281383
Sascha Gordon-Zolov <agordonzolov50@stuy.edu>
2024-09-17 11:42:31 -0400

    bitstreaming! read the file

--- 05_bitstream.py
+++
@@ -1,6 +0,0 @@
-'''Sascha Gordon-Zolov
-Team Sizzle
-Softdev
-K04:: Python Dictionaries and Random Selections
-2024-9-17
-Time spent: '''
---
+++ 05_stream/05_bitstream.py
@@ -0,0 +1,11 @@
+'''Sascha Gordon-Zolov
+Team Sizzle
+Softdev
+K04:: Python Dictionaries and Random Selections
+2024-9-17
+Time spent: '''
+from random import randint
+def bitstream():
+    file = open("krewes.txt", "r")
+    print(file.read())
+bitstream()
