0198616e98ff8a9f0e6b06883e3dba92137eb937
Sascha Gordon-Zolov <agordonzolov50@stuy.edu>
2024-09-18 11:38:28 -0400

    comments

--- 05_stream/05_bitstream.py
+++ 05_stream/05_bitstream.py
@@ -6,11 +6,11 @@ K04:: Python Dictionaries and Random Selections
 Time spent: 0.5'''
 import random
 def bitstream():
-    file = open("krewes.txt", "r")
+    file = open("krewes.txt", "r") #r is read
     file = file.read()
-    l1 = file.split("@@@")
+    l1 = file.split("@@@") #single array
     for i in range (0, len(l1) - 1):
-        l1[i] = l1[i].split("$$$")
+        l1[i] = l1[i].split("$$$") #double array
     r = random.randint(0, len(l1) - 1)
     return ("Period: " + l1[r][0] + ", Name: " + l1[r][1] + ", Ducky: " + l1[r][2])
 
