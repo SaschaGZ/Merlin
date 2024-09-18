44764d814671021f74613120888023e6ddeb4254
Sascha Gordon-Zolov <agordonzolov50@stuy.edu>
2024-09-18 11:26:02 -0400

    final commits

--- 05_stream/05_bitstream.py
+++ 05_stream/05_bitstream.py
@@ -4,12 +4,14 @@ Softdev
 K04:: Python Dictionaries and Random Selections
 2024-9-17
 Time spent: '''
-from random import randint
+from random
 def bitstream():
     file = open("krewes.txt", "r")
     file = file.read()
     l1 = file.split("@@@")
     for i in range (0, len(l1) - 1):
         l1[i] = l1[i].split("$$$")
-    print(l1)
-bitstream()
+    r = random.randint(0, len(l1) - 1)
+    return ("Period: " + li[r][0] + "Name: " + li[r][1] + "Ducky: " + li[r][2])
+
+print(bitstream())
