e2b8b33cf2b38b6e58ada9b4ef2118387dda6a50
Sascha Gordon-Zolov <agordonzolov50@stuy.edu>
2024-09-18 11:27:52 -0400

    wrong txt

--- 05_stream/05_bitstream.py
+++ 05_stream/05_bitstream.py
@@ -4,7 +4,7 @@ Softdev
 K04:: Python Dictionaries and Random Selections
 2024-9-17
 Time spent: '''
-from random
+import random
 def bitstream():
     file = open("krewes.txt", "r")
     file = file.read()
@@ -12,6 +12,6 @@ def bitstream():
     for i in range (0, len(l1) - 1):
         l1[i] = l1[i].split("$$$")
     r = random.randint(0, len(l1) - 1)
-    return ("Period: " + li[r][0] + "Name: " + li[r][1] + "Ducky: " + li[r][2])
+    return ("Period: " + l1[r][0] + ", Name: " + l1[r][1] + ", Ducky: " + l1[r][2])
 
 print(bitstream())
