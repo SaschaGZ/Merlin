ab6bd612777ff69aa62374bf56da03f25f7a7372
Sascha Gordon-Zolov <143219346+SaschaGZ@users.noreply.github.com>
2024-10-25 22:09:42 -0400

    Consistent with Team Sizzle

--- 19_dbo/build_db.py
+++ 19_dbo/build_db.py
@@ -1,12 +1,26 @@
 import csv
 import sqlite3
 
+db = sqlite3.connect("foo")
+c = db.cursor()
+c.execute("CREATE TABLE students(name TEXT, age INTEGER, userid INTEGER)")
+c.execute("CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER)")
 with open('students.csv', newline = '') as csvfile:
     reader = csv.DictReader(csvfile)
-    db = sqlite3.connect("foo")
-    c = db.cursor()
-    #c.execute("CREATE TABLE roster(name TEXT, userid INTEGER, age INTEGER)")
     for line in reader:
-        c.execute("INSERT INTO roster (name, userid, age) VALUES (line[("name")], line[("userid")], line[("age")])")
-    db.commit()
-    db.close()
+        name = str(line['name'])
+        age = int(line['age'])
+        id = int(line['id'])
+        print("INSERT INTO students (name, age, userid) " + f"VALUES ('{name}', {age}, {id})")
+        c.execute("INSERT INTO students (name, age, userid) " + f"VALUES ('{name}', {age}, {id})")
+        # c.execute("INSERT INTO roster(name) VALUES (?)", line['name'])
+with open('courses.csv', newline = '') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for line in reader:
+        code = str(line['code'])
+        mark = int(line['mark'])
+        id = int(line['id'])
+        print("INSERT INTO courses (code, mark, id) " + f"VALUES ('{code}', {mark}, {id})")
+        c.execute("INSERT INTO courses (code, mark, id) " + f"VALUES ('{code}', {mark}, {id})")
+db.commit()
+db.close()
