26d4c001f1a54ae99eea47b334ca88bf05482ff1
Sascha Gordon-Zolov <agordonzolov50@stuy.edu>
2025-03-13 11:01:17 -0400

    update

--- k35/app/__init__.py
+++ k35/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
@@ -24,6 +24,10 @@ def load_user(user_id):
             return user
     return None
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -33,7 +37,19 @@ def login():
             if user.username == username and user.password == password:
                 login_user(user)
                 return redirect(url_for('protected'))
+        flash('Invalid username or password')
     return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        new_user = User(id=len(users) + 1, username=username, password=password)
+        users.append(new_user)
+        flash('Registration successful, please log in')
+        return redirect(url_for('login'))
+    return render_template('register.html')
 
 @app.route('/protected')
 @login_required
